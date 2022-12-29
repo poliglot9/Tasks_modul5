119eb0454e9930d0d59bcc1b3280858691a28cdb
poliglot9 <119806034+poliglot9@users.noreply.github.com>
2022-12-29 17:34:36 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,10 @@
+let promt = (+ '123')
+console.log (promt)
+console.log (typeof 123)
+const number = prompt("Введите число для проверки:123");
+if(number% 2 == 0) {
+console.log("Это четное число");
+} else {
+console.log("Это нечетное число");
+}
+console.log(typeof NaN)
